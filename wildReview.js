debfacfd0d049e4e08872c71b6ab1d8a66366c9b
David Ascherl <dayved282gmail.com>
2022-08-22 10:39:45 -0500

    Added skill checker that works

--- wildReview.js
+++ wildReview.js
@@ -1,3 +1,4 @@
+//stats level and moon circle
 var intel = 0;
 var wis = 0;
 var cha = 0;
@@ -5,17 +6,39 @@ var lvl = 0;
 var moon = false;
 var maxCR = 0;
 
+//skills
+var skillList = ["Acrobatics", "Animal Handling", "Arcana", "Athletics", "Deception", "History", "Insight", "Intimidation", "Investigation",
+                 "Medicine", "Nature", "Perception", "Performance", "Persuasion", "Religion", "Sleight of Hand", "Stealth", "Survival"];
+var skillChecked = [false, false, false, false, false, false, false, false, false, 
+                    false, false, false, false, false, false, false, false,];
+
 $(document).ready(initApplication);
 
-console.log(animals);
+//function to see if things are checked
+function check(skill){
+    return document.getElementById(skill).checked;
+}
+
+//funtion to get elements
+function getEl(thing){
+    return document.getElementById(thing).value
+ }
+
+ //function to check for checked skills
+function getSkills(skillList){
+    for(var i = 0; i < skillList.length; i++){
+        skillChecked[i] = check(skillList[i]);
+    }
+    }
 
 function initApplication(){
+    
     document.getElementById("submit").addEventListener("click", function(){
-    var tempIntel = parseInt(document.getElementById("int").value);
-    var tempWis = parseInt(document.getElementById("wis").value);
-    var tempCha = parseInt(document.getElementById("cha").value);
-    var tempLvl = parseInt(document.getElementById("lvl").value);
-    var tempMoon = document.getElementById("moon").checked;
+    var tempIntel = parseInt(getEl("int"));
+    var tempWis = parseInt(getEl("wis"));
+    var tempCha = parseInt(getEl("cha"));
+    var tempLvl = parseInt(getEl("lvl"));
+    var tempMoon = check("moon");
     
     //check int wis and cha for numbers
     if (isNaN(tempIntel) || isNaN(tempWis) || isNaN(tempCha)){
@@ -77,5 +100,10 @@ function initApplication(){
 
     console.log("CR: " + findMaxCR(lvl));
     maxCR = findMaxCR(lvl);
+    getSkills(skillList);
+    for(var i = 0; i < skillList.length; i++){
+        console.log(skillList[i] + ": " + skillChecked[i]);
+    }
 });
 }
+
